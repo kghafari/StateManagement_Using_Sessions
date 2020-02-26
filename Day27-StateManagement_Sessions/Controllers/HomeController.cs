3199fe3ceaac036741d1d0edae6fca7155458f25
kghafari <kghafari@users.noreply.github.com>
2020-02-25 22:14:16 -0500

    push

---
+++ Day27-StateManagement_Sessions/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Day27_StateManagement_Sessions.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Day27_StateManagement_Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SaveUserName(string userName)
+        {
+            //session magic:
+            //Going into the HttpContext Class, to the Session property
+            //using the STATIC method called SetString to add a key value
+            //pair to our Session class
+            HttpContext.Session.SetString("SessionName", userName); //creates a NEW session
+            return View("Index");
+        }
+
+    }
+}
+
+//PUT THIS IN STARTUP UNDER ConfigureServices to enable/use sessions
+//services.AddSession(
+//                options =>
+//                {
+//                    options.IdleTimeout = TimeSpan.FromSeconds(10);
+//                    options.Cookie.HttpOnly = true;
+//                    options.Cookie.IsEssential = true;
+//                }
+//            );
+
+
+//// add this for reasons in the Configure method (under startup)
+//app.UseSession();
---
+++ Day27-StateManagement_Sessions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Day27_StateManagement_Sessions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
